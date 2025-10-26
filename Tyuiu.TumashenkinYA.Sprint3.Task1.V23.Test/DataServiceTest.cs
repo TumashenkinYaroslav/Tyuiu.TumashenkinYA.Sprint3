f1eb741c67888233e55802ca4f8b1c6c811c37c9
eborius321 <eborius321@gmail.com>
2025-10-26 19:54:44 +0500

    Task1

---
+++ Tyuiu.TumashenkinYA.Sprint3.Task1.V23.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.TumashenkinYA.Sprint3.Task1.V23.Lib;
+namespace Tyuiu.TumashenkinYA.Sprint3.Task1.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 5;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 0.071;//245.155;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.TumashenkinYA.Sprint3.Task1.V23/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.TumashenkinYA.Sprint3.Task1.V23.Lib;
+
+namespace Tyuiu.TumashenkinYA.Sprint3.Task1.V23
+{
+    class Program
+    {
+        static void Main()
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("************************************************************************");
+
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 5;
+            Console.WriteLine("X:" + value);
+            Console.WriteLine("старт шага:" + startValue);
+            Console.WriteLine("конец шага:" + stopValue);
+
+
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+            Console.WriteLine("************************************************************************");
+
+            Console.WriteLine($"Ответ = " + ds.GetMultiplySeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
